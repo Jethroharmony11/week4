20d1b2fc1446d6eb600c82ed3c730aba2743c9f6
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-23 13:55:19 -0500

    function2

---
+++ more_funtions.js
@@ -0,0 +1,21 @@
+//functionmore_functions.js//displayMailinglable()
+//nameaddresscitystatezipconsole.log()
+//addnumbers()num1num2
+// somenumber+somenumber =somenumber displayreceipt()totaldueamountPaid
+//total Due: $somenumber
+//amount Paid: $someNumber
+//change Due: $someNumber
+//? displayMailinglable()
+//? addNumbers()
+//displayreceipt()
+
+
+// 1st
+function displayMailinglable(name, address, city, state, zip)
+console.log(name);
+console.log(address);
+console.log(city +","+state + " "+ zip);// +contenate -chained together
+// console.log(`${city}, ${state}, )
+
+    )
+)
