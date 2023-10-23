269671e0ae8ab0dc9fc4c4a4378d9ecc10b421a0
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-23 11:13:06 -0500

    first commit

---
+++ Demo_function.js
@@ -0,0 +1,8 @@
+
+//DEFINE THE FUNCTOIN
+function greet(name, kind){
+    return `${kind}, ${name}.`, //Use back-tick js template strings and expressions
+    //return kind + " " + name,
+    }
+    //CALL THE Function AND PASSING THE RESULT TO LOG
+    console.log(greet("kevin"))
